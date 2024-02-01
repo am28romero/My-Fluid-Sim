e19ea1e8bc0f3202ab966fd095cb689f37c315e5
am28romero <am28romero@gmail.com>
2024-02-01 00:44:30 -0600

    Working on Sim Compute Shader

--- Assets/Scripts/ParticleRenderer.cs
+++ Assets/Scripts/ParticleRenderer.cs
@@ -19,7 +19,7 @@ public class ParticleRenderer : MonoBehaviour
     public float sdfThresh = 0.0f;
     [Range(0.0f, 2.0f)]
     public float smoothingCoef = 0.1f;
-    private Simulation sim;
+    [SerializeField] private Simulation sim;
 
     public ParticleRenderer(Vector2[] positions)
     {
@@ -30,7 +30,7 @@ public class ParticleRenderer : MonoBehaviour
         }
         this.positions = positions;
         CreateCircle();
-        // sim = GetComponent<Simulation>();
+        sim = GetComponent<Simulation>();
     }
 
     public void CreateCircle()
@@ -99,12 +99,12 @@ public class ParticleRenderer : MonoBehaviour
             }
             posTex.Apply();
 
-            // if (sim.DebugCode << 1 == 1) { 
-            //     Debug.Log("1" + posTex.GetPixel(0, 0));
-            //     Debug.Log("2" + posTex.GetPixel(1, 0));
-            //     Debug.Log("3" + posTex.GetPixel(2, 0));
-            //     Debug.Log("4" + posTex.GetPixel(3, 0));
-            // }
+            if ((sim.DebugCode >> 1 & 1) == 1) { 
+                Debug.Log("1" + posTex.GetPixel(0, 0));
+                Debug.Log("2" + posTex.GetPixel(1, 0));
+                Debug.Log("3" + posTex.GetPixel(2, 0));
+                Debug.Log("4" + posTex.GetPixel(3, 0));
+            }
             
             meshRenderer.material.SetFloat("_Radius", radius);
             meshRenderer.material.SetInt("_NumPositions", positions.Length);
@@ -122,9 +122,9 @@ public class ParticleRenderer : MonoBehaviour
     public void SetPositions(Vector2[] pos)
     {
         positions = pos;
-        // if (sim.DebugCode << 2 == 1) { 
-        //     Debug.Log(pos.Length + "\n" + pos[0]);
-        //     Debug.Log(positions.Length + "\n" + positions[0]);
-        // }
+        if ((sim.DebugCode >> 2 & 1) == 1) { 
+            Debug.Log(pos.Length + "\n" + pos[0]);
+            Debug.Log(positions.Length + "\n" + positions[0]);
+        }
     }
 }
--- Assets/Scripts/Simulation.cs
+++ Assets/Scripts/Simulation.cs
@@ -16,11 +16,11 @@ public class Simulation : MonoBehaviour
     [SerializeField] private bool spawnParticles = true; // Whether to spawn particles
     [SerializeField] private Vector2[] positions = {}; // Positions of particles
     [SerializeField] private Vector2[] velocities = {}; // Velocities of particles
-    [SerializeField] private float gravity = 9.8f; // Gravity strength
+    [SerializeField] private Vector2 gravity = new(0, 9.8f); // Gravity strength
     [SerializeField] private float timeStep = 1.0f; // Time step
     [SerializeField] private uint iterations = 1; // Number of iterations per fram
     [SerializeField] private float collisionDamping = 0.9f; // Damping factor for collisions
-    [SerializeField] private uint debugCode; // Debug code
+    [SerializeField] private uint debugCode = 0b1111; // Debug code
     private int kernelIndex; // Index of kernel in compute shader
     private ComputeBuffer positionBuffer; // Buffer for positions
     private ComputeBuffer velocityBuffer; // Buffer for velocities
@@ -48,17 +48,12 @@ public class Simulation : MonoBehaviour
         spawner = GetComponent<ParticleSpawner>();
         boxCollider = GetComponent<BoxCollider2D>();
         
-        if (DebugCode << 0 == 1) { Debug.Log(pRenderer.positions.Length); }
+        if ((DebugCode & 1) == 1) { Debug.Log(pRenderer.positions.Length); }
 
         if (positions.Length == 0 || spawnParticles) {
             spawnData = spawner.GetSpawnData((int) particleCount);
             positions = spawnData.positions;
             velocities = spawnData.velocities;
-
-            // for (int i = velocities.Length; i < positions.Length; i++)
-            // {
-            //     velocities[i] = new Vector2(0.0f, 0.0f);
-            // }
         }
 
         // Get kernel index and create buffers
@@ -77,58 +72,75 @@ public class Simulation : MonoBehaviour
         // Update timer
         Time.timeScale = timeStep;
         timer += Time.deltaTime;
-        Vector2[] velocitiesBuffer = new Vector2[positions.Length];
+        Vector2[] velos = new Vector2[positions.Length];
 
         // Get box collider size
         float topCollider = boxCollider.bounds.max.y;
         float bottomCollider = boxCollider.bounds.min.y;
         float leftCollider = boxCollider.bounds.min.x;
         float rightCollider = boxCollider.bounds.max.x;
-
-        // Update positions and velocities in compute shader
-        // positionBuffer.SetData(positions);
-        // velocityBuffer.SetData(velocities);
-        // simShader.SetFloat("_DeltaTime", Time.deltaTime);
-
-        // simShader.SetBuffer(kernelIndex, "_positions", positionBuffer);
-        // simShader.SetBuffer(kernelIndex, "_velocities", velocityBuffer);
-
-        // Update positions and velocities
-        for (int i = 0; i < positions.Length; i++)
+        for (int i = 1; i <= iterations; i++)
         {
-            // Update velocity
-            // velocities[i] *= 0.8f; // Damping factor
-            if (i < velocities.Length) velocitiesBuffer[i] = velocities[i];
-            velocitiesBuffer[i] += new Vector2(0.0f, -gravity * Time.deltaTime);
+            // Update positions and velocities in compute shader
+            positionBuffer.SetData(Vector2ToFloat2(positions));
+            velocityBuffer.SetData(Vector2ToFloat2(velocities));
+            simShader.SetFloat("_DeltaTime", Time.deltaTime/i);
+            simShader.SetInt("_NumParticles", positions.Length);
+            simShader.SetVector("_Gravity", gravity);
 
-            // Update position
-            positions[i] += velocitiesBuffer[i] * Time.deltaTime;
+            simShader.SetBuffer(kernelIndex, "_positions", positionBuffer);
+            simShader.SetBuffer(kernelIndex, "_velocities", velocityBuffer);
 
-            // Check for collision with ground
-            if (positions[i].y < bottomCollider)
-            {
-                positions[i].y = bottomCollider;
-                velocitiesBuffer[i].y *= -collisionDamping;
-            } else if (positions[i].y > topCollider)
-            {
-                positions[i].y = topCollider;
-                velocitiesBuffer[i].y *= -collisionDamping;
-            } else if (positions[i].x < leftCollider)
-            {
-                positions[i].x = leftCollider;
-                velocitiesBuffer[i].x *= -collisionDamping;
-            } else if (positions[i].x > rightCollider)
-            {
-                positions[i].x = rightCollider;
-                velocitiesBuffer[i].x *= -collisionDamping;
-            }
+            // Update positions and velocities
+            simShader.Dispatch(kernelIndex, positions.Length, 1, 1);
+            positionBuffer.GetData(positions);
+            velocityBuffer.GetData(velocities);
         }
-        velocities = velocitiesBuffer;
+        // for (int i = 0; i < positions.Length; i++)
+        // {
+        //     // Update velocity
+        //     // velocities[i] *= 0.8f; // Damping factor
+        //     if (i < velocities.Length) velos[i] = velocities[i];
+        //     velos[i] += new Vector2(0.0f, -gravity * Time.deltaTime);
+
+        //     // Update position
+        //     positions[i] += velos[i] * Time.deltaTime;
+
+        //     // Check for collision with ground
+        //     if (positions[i].y < bottomCollider)
+        //     {
+        //         positions[i].y = bottomCollider;
+        //         velos[i].y *= -collisionDamping;
+        //     } else if (positions[i].y > topCollider)
+        //     {
+        //         positions[i].y = topCollider;
+        //         velos[i].y *= -collisionDamping;
+        //     } else if (positions[i].x < leftCollider)
+        //     {
+        //         positions[i].x = leftCollider;
+        //         velos[i].x *= -collisionDamping;
+        //     } else if (positions[i].x > rightCollider)
+        //     {
+        //         positions[i].x = rightCollider;
+        //         velos[i].x *= -collisionDamping;
+        //     }
+        // }
+        // velocities = velos;
 
         // Update positions in renderer
         pRenderer.positions = positions;
-        if (DebugCode << 0 == 1) { Debug.Log(pRenderer.positions.Length); }
+        if ((DebugCode & 1) == 1) { Debug.Log(pRenderer.positions.Length); }
 
         pRenderer.UpdateCircle();
+    }
+
+    float2[] Vector2ToFloat2(Vector2[] vec2)
+    {
+        float2[] f2 = new float2[vec2.Length];
+        for (int i = 0; i < vec2.Length; i++)
+        {
+            f2[i] = new float2(vec2[i].x, vec2[i].y);
+        }
+        return f2;
     }
 }
